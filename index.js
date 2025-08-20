77fe85c9a9db4512c6e00895eb9ce312b494256f
ahmad zia <adv.ahmad.zia@gmail.com>
2025-08-20 12:05:17 -0700

    neww updatess

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+
+// Serve HTML from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/initiate', require('./lib/initiate'));
-
 app.get('/refresh', require('./lib/refresh'));
-
 app.get('/oauth/callback', require('./lib/callback'));
 
-app.listen(3000, () => {
-    console.log("App Listening on 3000 !");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("App Listening on port", process.env.PORT || 3000);
 });
